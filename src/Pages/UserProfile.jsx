7a72e23594ee746618671f501fe81b118f527e40
monishaRema <monisharema97@gmail.com>
2025-06-11 14:46:31 +0600

    Profile page basic design

--- src/Pages/UserProfile.jsx
+++ src/Pages/UserProfile.jsx
@@ -3,6 +3,7 @@ import { AuthContext } from '../Contex/AuthContex';
 
 const UserProfile = () => {
     const {user} = use(AuthContext)
+    console.log(user)
     if(!user?.email){
         location.reload();
     }
@@ -12,9 +13,9 @@ const UserProfile = () => {
        <section className='py-25 bg-gradient-to-r from-primary to-green-800'>
 <div className="container mx-auto px-5">
     <div className="flex flex-col md:flex-row">
-        <div className="profile-info">
+        <div className="profile-info bg-[#00000030] rounded p-5 w-full md:w-4/12">
             <div className="img-box">
-                <img src={user?.photo_url} alt={`${user?.displayName} image`} />
+                <img src={user?.photoURL} alt={`${user?.displayName} image`} className='size-20 rounded-full mx-auto' />
             </div>
         </div>
         <div className="profile-details">
