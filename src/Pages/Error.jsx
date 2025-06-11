11448b7439987a3c768ce47fcf7496425979d3c5
monishaRema <monisharema97@gmail.com>
2025-06-11 14:27:53 +0600

    404 page designed modified

--- src/Pages/Error.jsx
+++ src/Pages/Error.jsx
@@ -3,38 +3,27 @@ import React from "react";
 import { FaArrowLeft } from "react-icons/fa";
 import { Link } from "react-router";
 import { motion } from "motion/react";
+import Lottie from "lottie-react";
+import ErrorLottie from "../assets/Lottie/Error.json"
 
 const Error = () => {
   return (
-    <section className="register py-25">
+    <section className="register py-25 ">
       <Helmet>
         <title>Book Case | 404</title>
       </Helmet>
       <div className="container mx-auto px-5 text-center">
-        <motion.h1
-          initial={{ y: 0, opacity: 0.5, scale: 0.95 }}
-          animate={{
-            y: [-5, -20, -5],
-            opacity: [0.5, 1, 0.5],
-            scale: [0.95, 1, 0.95],
-            color: ["#00ED6399", "#00ED64", "#FFFFFF", "#00ED6399"]
-          }}
-          transition={{
-            duration: 2.5,
-            repeat: Infinity,
-            repeatType: "loop",
-            ease: "easeInOut",
-          }}
-          className="text-center text-[120px] mb-10"
-        >
-          404
-        </motion.h1>
+        <div className="max-w-3xl mx-auto">
+               <Lottie
+              style={{ width: "100%", height: "100%" }}
+              animationData={ErrorLottie}
+              loop={true}
+              ></Lottie>
+              </div>
 
-        <p className="text-xl mb-10">
-          Oops! Page not found <br /> The page you're looking for seems to have
-          wandered off into the digital void.{" "}
+        <p className="text-xl mt-10 mb-5 text-gray-300">
+          <span>Oops! Page not found </span><br /> The page you're looking for seems doesn't exist. 
         </p>
-
         <div className="flex justify-center">
           <Link to="/" className="gradient-btn flex items-center gap-3">
             <FaArrowLeft className="text-xl" />
