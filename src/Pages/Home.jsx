bb33a01b39c4203a66dfd752809d629387497dd7
monishaRema <monisharema97@gmail.com>
2025-06-15 18:40:01 +0600

    Hero Section Modified

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import Banner from '../Components/Header/Banner';
 import { Helmet } from '@dr.pogodin/react-helmet';
 import PopularBooks from '../Components/Home/PopularBooks';
 import RecentBooks from '../Components/Home/RecentBooks';
 import Category from '../Components/Home/Category';
 import CTA from '../Components/Home/CTA';
+import Hero from '../Components/Header/Hero';
 
 const Home = () => {
       
@@ -14,7 +14,7 @@ const Home = () => {
       <title>Book Case | Home
       </title>
     </Helmet>
-       <Banner></Banner>
+       <Hero></Hero>
        <PopularBooks></PopularBooks>
        <Category></Category>
        <CTA></CTA>
